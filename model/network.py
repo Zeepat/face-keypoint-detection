225392a7900449bfa9a368d1e5659d0a9cb02493
Asajad Hussein <firstzpat@gmail.com>
2025-01-10 16:06:54 +0100

    Updated network to add normalisation layers and bboxes training

---
+++ model/network.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        
+        self.conv1 = nn.Conv2d(3, 32, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.conv5 = nn.Conv2d(256, 512, 1)
+        
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.bn5 = nn.BatchNorm2d(512)
+
+        self.fc1 = nn.Linear(512, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        
+        # Separate heads for keypoints and bounding box
+        self.fc_keypoints = nn.Linear(512, 8)  # 4 keypoints * 2 (x, y)
+        self.fc_bbox = nn.Linear(512, 4)      # Bounding box (x_min, y_min, x_max, y_max)
+        
+        self.drop1 = nn.Dropout(p=0.25)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+        x = self.pool(F.relu(self.bn5(self.conv5(x))))
+
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.drop1(x)
+        x = F.relu(self.fc2(x))
+        x = self.drop1(x)
+        
+        # Separate outputs
+        keypoints = self.fc_keypoints(x)
+        bbox = self.fc_bbox(x)
+        
+        return keypoints, bbox
